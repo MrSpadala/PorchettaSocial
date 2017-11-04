a8c54b0b026b6dce2867e1600a5eede9876efa07
Spadala <pietro.spa@gmail.com>
2017-11-04 18:26:55 +0100

    Caricato programma di prova dei websockets

---
+++ Examples/websockets/server.js
@@ -0,0 +1,64 @@
+
+var WebSocketServer = require('websocket').server;
+var http = require('http');
+
+
+// il client deve richiedere lo stesso protocollo 
+var protocol = 'my-protocol'
+
+ 
+var server = http.createServer(function(request, response) {
+    console.log((new Date()) + ' Received request for ' + request.url);
+    response.writeHead(404);
+    response.end();
+});
+
+server.listen(8080, function() {
+    console.log((new Date()) + ' Server is listening on port 8080');
+});
+ 
+wsServer = new WebSocketServer({
+    httpServer: server,
+    // You should not use autoAcceptConnections for production
+    // applications, as it defeats all standard cross-origin protection
+    // facilities built into the protocol and the browser.  You should
+    // *always* verify the connection's origin and decide whether or not
+    // to accept it.
+    autoAcceptConnections: false
+});
+ 
+
+function originIsAllowed(origin) {
+  // put logic here to detect whether the specified origin is allowed.
+  return true;
+}
+
+var c = 0
+wsServer.on('request', function(request) {
+    if (!originIsAllowed(request.origin)) {
+      // Make sure we only accept requests from an allowed origin
+      request.reject();
+      console.log((new Date()) + ' Connection from origin ' + request.origin + ' rejected.');
+      return;
+    }
+    
+	var i = 0
+    var connection = request.accept(protocol, request.origin);
+    console.log((new Date()) + ' Connection accepted.');
+	connection.sendUTF('you are '+c.toString());
+	c++
+    connection.on('message', function(message) {
+        if (message.type === 'utf8') {
+            console.log('Received Message: ' + message.utf8Data);
+            connection.sendUTF(i.toString());
+			i++
+        }
+    });
+    connection.on('close', function(reasonCode, description) {
+        console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
+    });
+});
+
+
+
+
