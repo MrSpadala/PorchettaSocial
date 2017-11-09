7e53da27934a17fed7e262099a8ee23fa88503eb
theroggio <33184864+theroggio@users.noreply.github.com>
2017-11-09 14:49:19 +0100

    Create tumblr_queue.js
    Creata una bozza della funzione che legge da tumblr_queue, e in relazione al cmd nel messaggio fa autenticazione o post del messaggio. Da inserire la valutazione dei parametri opzionali e metodo completo API per la POST.

---
+++ Tumblr/tumblr_queue.js
@@ -0,0 +1,45 @@
+var amqp = require('amqplib/callback_api');
+
+function readPostAndReply() {
+amqp.connect( /*main server*/, function(err, conn) {
+				conn.createChannel(function(err,ch) {
+						
+            var tumblr_queue = ‘tmb’;
+            var to_server_queue = 'to_server';
+
+            ch.assertQueue(to_server_queue, {durable:true});
+						ch.assertQueue(tumblr_queue, {durable:true, replyTo: to_server_queue});
+						
+						ch.consume(tumblr_queue, function(msg){
+                var splitted_msg = msg.split('xFF');
+                
+                switch (splitted_msg[0]) { 
+                  case "auth": //it means the comand is the authentication of this social
+                               //redirect to tumblr.py for the authentication
+                               break;
+                               
+                  case "upload_post":  var text = splitted_msg[1]; //da RPC_FORMAT il primo parametro è il testo
+                                       var access_token = splitted_msg[2];
+                                       var access_token2 = splitted_msg[3]; //secondo access token, tumblr usa oAuth1
+                                       
+                                       // bisognerebbe fare un ciclo fino a splitted_msg.length e prendere tutti i parametri
+                                       // con uno switch che ti identifica ogni parametro cosa è visto che sono opzionali
+                                       // in questo modo ho acess token, testo, parametri
+                                       
+                                       tumblr_post_method = "https://api.tumblr.com/v2/blog/"+blog_id+"/post";
+                                       // va aggiunta ?api_key=chiave&text=testo e tutto il resto
+                                       
+                                       //la risposta generata in formato 201:Created o Error Code viene inviata al to_server
+                   }
+                                       
+
+		            }, {noAck: true});
+
+						
+				});
+		});
+}
+
+module.exports = { 
+consume : readPostAndReply
+}
