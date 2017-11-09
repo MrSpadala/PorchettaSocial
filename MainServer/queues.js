a790a8bb75df5d892898d3bd00df547dd8cd9324
theroggio <33184864+theroggio@users.noreply.github.com>
2017-11-09 13:36:16 +0100

    Eliminate assert e bind superflui
    Eliminate le code dichiarate nel server non necessarie, in invio vedo solo l'exchange e dichiaro la coda di ritorno per settarla nel valore replyTo di tutti i messaggi. In ricezione vedo solo la coda to_server.

--- MainServer/queues.js
+++ MainServer/queues.js
@@ -16,10 +16,6 @@ function sendToQueues(msg, network_list) {
 
   amqp.connect(/*main server*/, function(err,conn) {
     conn.createChannel(function(err,ch) {
-	    var fb_queue = 'fb';
-      	    var g_queue = 'g+';
-           var twt_queue = 'twt';
-	    var tumblr_queue = 'tmb';
     
       var to_server_queue = 'to_server';
     
@@ -27,25 +23,12 @@ function sendToQueues(msg, network_list) {
     
       ch.assertExchange(ex,'direct', {durable: true});
       ch.assertQueue(to_server_queue, {durable: true});
-      ch.assertQueue(fb_queue, {durable: true, reply_to: to_server_queue});
-      ch.assertQueue(g_queue, {durable: true, reply_to: to_server_queue});
-      ch.assertQueue(twt_queue, {durable: true, reply_to: to_server_queue});
-      ch.assertQueue(tumblr_queue, {durable: true, reply_to: to_server_queue});
-   
-      ch.bindQueue(fb_queue,ex,'fb');
-      // l'ultimo parametro è la routing_key, in questo modo una post con routing key = 'fb' va sullacoda fb_queue
-      ch.bindQueue(g_queue,ex,'g+');
-      ch.bindQueue(twt_queue,ex,'twt');
-      ch.bindQueue(tumblr_queue,ex,'tmb');
-
-      //se facciamo una cosa carina, che tipo i nomi dei social in network_list è uguale alla routing_key del social
-      //possiamo prendere direttamente i valori da li e usarli nella publish 
 	
 	var correlation_id_list = new Array;
 	    
 	for (var k = 0; k < network_list.length; k++){
 	      var msg_id = globals.request_msg_id()
-	      ch.publish(ex, network_list[k] , new Buffer(msg), {correlationId: msg_id});
+	      ch.publish(ex, network_list[k] , new Buffer(msg), {correlationId: msg_id, replyTo: to_server_queue});
 	      correlation_id_list.push(msg_id);
 	      log('Sent msg_id:'+ msg_id + msg ); 
 	}
