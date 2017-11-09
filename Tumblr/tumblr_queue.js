a4f3356c81c28e7355624f14bba24b6290ce890f
theroggio <33184864+theroggio@users.noreply.github.com>
2017-11-09 21:32:32 +0100

    Update tumblr_queue.js
    - Eliminato lo switch case ( senza il comando 'auth' l'unico comando è posta quindi viene eseguito in automatico)
    - Da modificare la numerazione dei parametri se si esclude il parametro 'cmd' dal formato dei messaggi

--- Tumblr/tumblr_queue.js
+++ Tumblr/tumblr_queue.js
@@ -2,7 +2,7 @@ var amqp = require('amqplib/callback_api');
 
 function readPostAndReply() {
 amqp.connect('amqp://rabbit-mq', function(err, conn) {
-				conn.createChannel(function(err,ch) {
+	     conn.createChannel(function(err,ch) {
 						
             var tumblr_queue = ‘tmb’;
             var to_server_queue = 'to_server';
@@ -12,34 +12,24 @@ amqp.connect('amqp://rabbit-mq', function(err, conn) {
 						
 	    ch.consume(tumblr_queue, function(msg){
                 var splitted_msg = msg.split('xFF');
-                
-                switch (splitted_msg[0]) { 
-                  case "auth": //it means the comand is the authentication of this social
-                               //redirect to tumblr.py for the authentication
-                               break;
-                               
-                  case "upload_post":  var text = splitted_msg[1]; //da RPC_FORMAT il primo parametro è il testo
-                                       var access_token = splitted_msg[2];
-                                       var access_token2 = splitted_msg[3]; //secondo access token, tumblr usa oAuth1
+                                           
+                  var text = splitted_msg[1]; //da RPC_FORMAT il primo parametro è il testo
+                  var access_token = splitted_msg[2];
+                  var access_token2 = splitted_msg[3]; //secondo access token, tumblr usa oAuth1
                                        
-                                       // bisognerebbe fare un ciclo fino a splitted_msg.length e prendere tutti i parametri
-					// uno dei parametri deve essere il msg_id
-                                       // con uno switch che ti identifica ogni parametro cosa è visto che sono opzionali
-                                       // in questo modo ho acess token, testo, parametri
+                  // bisognerebbe fare un ciclo fino a splitted_msg.length e prendere tutti i parametri
+		  // uno dei parametri deve essere il msg_id
+                  // con uno switch che ti identifica ogni parametro cosa è visto che sono opzionali
+                  // in questo modo ho acess token, testo, parametri
                                        
-                                       tumblr_post_method = "https://api.tumblr.com/v2/blog/"+blog_id+"/post";
-                                       // va aggiunta ?api_key=chiave&text=testo e tutto il resto
+                  tumblr_post_method = "https://api.tumblr.com/v2/blog/"+blog_id+"/post";
+                  // va aggiunta ?api_key=chiave&text=testo e tutto il resto
                                        
-                                       //la risposta generata in formato 201:Created o Error Code viene formattata inserendo il msg_id
-					ch.sendToQueue(to_server_queue,/* response*/);
-                   }
-                                       
-
-		            }, {noAck: true});
-
-						
-				});
-		});
+                  //la risposta generata in formato 201:Created o Error Code viene formattata inserendo il msg_id
+		  ch.sendToQueue(to_server_queue,/* response*/);
+	    }, {noAck: true});
+	});
+    });
 }
 
 module.exports = { 
