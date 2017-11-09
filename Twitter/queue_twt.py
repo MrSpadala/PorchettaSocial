00f4c571a242e75794b229ff9b24b21fb41e90b8
erre4 <riciric83@gmail.com>
2017-11-09 21:52:41 +0100

    modificata queue_twt.py con msg_id

--- Twitter/queue_twt.py
+++ Twitter/queue_twt.py
@@ -20,7 +20,8 @@ def callback(ch, method, properties, body):
 		print(e)
 	'''
 	flag = 'ÿ'
-	if (l[0] == 'auth'):
+	msg = l[0]
+	if (l[1] == 'auth'):
 		twitter = OAuth1Service(name='twitter',
                         consumer_key='VyP9pdp6VC1M0qkfS4m14oxqM',
                         consumer_secret='udtYapVuIU3vFalBjRmHWIVPPE6yA9BK4Zwzj6XB1kRcg8ekQq',
@@ -32,11 +33,11 @@ def callback(ch, method, properties, body):
 
         authorize_url = twitter.get_authorize_url(request_token)
 		
-		stringa_invio ='twtÿauthÿ'+authorize_url+flag+request_token+flag+request_token_secret
+		stringa_invio =msg+flag+'twtÿauthÿ'+authorize_url+flag+request_token+flag+request_token_secret
 		#print("messaggio inviato:\n"stringa_invio)
 		channel1.basic_publish(exchange='',routing_key = 'to_server',body=stringa_invio)
 		
-	elif l[0] == 'verify_pin':
+	elif l[1] == 'verify_pin':
 		twitter = OAuth1Service(name='twitter',
                         consumer_key='VyP9pdp6VC1M0qkfS4m14oxqM',
                         consumer_secret='udtYapVuIU3vFalBjRmHWIVPPE6yA9BK4Zwzj6XB1kRcg8ekQq',
@@ -44,18 +45,18 @@ def callback(ch, method, properties, body):
                         access_token_url='https://api.twitter.com/oauth/access_token',
                         authorize_url='https://api.twitter.com/oauth/authorize')
                         
-        pin = l[1]                
-		request_token = l[2]
-		request_token_secret = l[3]
+        pin = l[2]                
+		request_token = l[3]
+		request_token_secret = l[4]
 		stringa_invio = ''
         try:
 			session = twitter.get_auth_session(request_token,request_token_secret,method='POST',data={'oauth_verifier': pin})
 			token1 = session.access_token
 			token2 = session.access_token_secret
-			stringa_invio ='twtÿverify_pinÿ'+token1+flag+token2
+			stringa_invio =msg+flag+'twtÿverify_pinÿ'+token1+flag+token2
 
 		except Exception:
-			stringa_invio ='twtÿverify_pinÿ'+'exception_occurred'
+			stringa_invio =msg+flag+'twtÿverify_pinÿ'+'exception_occurred'
 
 		#print("messaggio inviato:\n"stringa_invio)
 		channel1.basic_publish(exchange='',routing_key = 'to_server',body=stringa_invio)
@@ -63,18 +64,18 @@ def callback(ch, method, properties, body):
 	else:
 		consumer_key = 'VyP9pdp6VC1M0qkfS4m14oxqM'
 		consumer_secret = 'udtYapVuIU3vFalBjRmHWIVPPE6yA9BK4Zwzj6XB1kRcg8ekQq'
-		access_token = l[2]
-		access_token_secret = l[3]
+		access_token = l[3]
+		access_token_secret = l[4]
 		oauth = OAuth1Session(consumer_key, client_secret = consumer_secret,resource_owner_key = access_token,resource_owner_secret = access_token_secret)
 		params = {'status': 'testo'}
 
-		params['status']=l[1]
+		params['status']=l[2]
 		r = oauth.post('https://api.twitter.com/1.1/statuses/update.json', data = params,json=None)
 		rispota = 1
 		if ('200' in str(r)):
 			risposta = 0
 		
-		stringa_invio ='twtÿupload_postÿ'+risposta
+		stringa_invio =msg+flag+'twtÿupload_postÿ'+risposta
 		
 		#print("messaggio inviato:\n"stringa_invio)
 		channel1.basic_publish(exchange='',routing_key = 'to_server',body=stringa_invio)
