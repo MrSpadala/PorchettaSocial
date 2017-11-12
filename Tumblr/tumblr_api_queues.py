dcdca60a554fd477f8b3d1f8b024b7b68e16d95a
theroggio <33184864+theroggio@users.noreply.github.com>
2017-11-12 02:30:49 +0100

    Update tumblr_api_queues.py
    - Inserito corpo del elif command = upload_post, da terminare con invio risposta
    - Il CODE necessario ad avviare la sessione di Tumblr (analogo del pin di Twitter?) va passato (RPC FORMAT)

--- Tumblr/tumblr_api_queues.py
+++ Tumblr/tumblr_api_queues.py
@@ -23,8 +23,27 @@ def callback(ch, method, properties, body):
 	
 	if(command == 'auth' ):
 		"""TODO"""
+		# da prendere in tumblr_api.py
 	elif(command == 'upload_post'):
 		"""TODO"""
+		text = message[2] #da RPC_FORMAT il messaggio è msg_id|cmd|messaggio|token|...
+		access_token = message[3]
+		access_token2 = message[4] #secondo access token, tumblr usa oAuth1
+		
+		tumblr = OAuth1Service(name='tumblr', consumer_key='',consumer_secret='',request_token_url='',access_token_url='',authorize_url='')
+                session = tumblr.get_auth_session(access_token,access_token2,method='POST',data={'oauth_verifier': #codice, tocca passarlo?})
+		s = session.get('http://api.tumblr.com/v2/user/info').json()
+		blog_id = s['response']['user']['blog_id']
+		tumblr_post_string = "https://api.tumblr.com/v2/blog/"+blog_id+"/post"
+												 
+		params['body'] = text
+		s = session.post(tumblr_post_string, data = params, json=None).json()
+
+                #la risposta generata in formato 201:Created o Error Code viene formattata inserendo il msg_id = message[0]
+		#inviare in coda il messaggio di risposta qui
+		#channel.basic_publish(exchange='',routing_jey='to_server',body= // risposta // )
+
+		 
 	else :
 		"""TODO"""
 	
