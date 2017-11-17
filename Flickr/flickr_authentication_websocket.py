5a3a0f90eac46baf1dc7b4f5fe67c8d7b2c9fd0d
Francesco Terenzi <terenzi_francesco@yahoo.it>
2017-11-17 13:03:53 +0100

    Defnita la queue_fkr e l'autenticazione tramite ws

---
+++ Flickr/flickr_authentication_websocket.py
@@ -0,0 +1,51 @@
+import oauth2 as oauth
+import time
+import httplib2
+import urllib
+
+import asyncio
+import websockets
+
+import flickr_methods
+
+ip_address = '127.0.0.1'
+port = 12345
+	
+
+async def callback(websocket, path):
+	
+	stringa = await websocket.recv()
+	print(stringa)
+	
+	l = stringa.split('ÿ')
+	print("Received msg splitted ",l)
+
+	flag = 'ÿ'
+	name = 'fkr'
+	
+	if (l[0] == 'auth'):
+		request_token, request_token_secret = flickr_methods.get_request_token()
+		
+		stringa_invio =name+flag+'auth'+flag+authorize_url+flag+request_token+flag+request_token_secret
+		await websocket.send(stringa_invio)
+	
+	elif l[0] == 'verify_pin':
+
+		pin = l[1]                
+		request_token = l[2]
+		request_token_secret = l[3]
+		stringa_invio = ''
+		
+		try:
+			access_token, access_token_secret = flickr_methods.get_access_token(pin, request_token, request_token_secret)
+			stringa_invio = name+flag+'verify_pin'+flag+access_token+flag+access_token_secret
+
+		except Exception:
+			stringa_invio =name+flag+'verify_pin'+flag+'exception_occurred'
+
+		await websocket.send(stringa_invio)
+
+		
+start_server = websockets.serve(callback, ip_address, port)
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
