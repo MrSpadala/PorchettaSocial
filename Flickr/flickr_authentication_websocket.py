6cf2d126becad5e5249797d93ac1b226ecba6018
Francesco Terenzi <terenzi_francesco@yahoo.it>
2017-11-21 13:23:22 +0100

    Log on Flickr enabled

--- Flickr/flickr_authentication_websocket.py
+++ Flickr/flickr_authentication_websocket.py
@@ -32,8 +32,8 @@ async def callback(websocket, path):
 		stringa_invio = ''
 		
 		try:
-			access_token, access_token_secret = flickr_methods.get_access_token(pin, request_token, request_token_secret)
-			stringa_invio = name+flag+'verify_pin'+flag+access_token+flag+access_token_secret
+			token1, token2 = flickr_methods.get_access_token(pin, request_token, request_token_secret)
+			stringa_invio = name+flag+'verify_pin'+flag+token1+flag+token2
 
 		except Exception:
 			stringa_invio =name+flag+'verify_pin'+flag+'exception_occurred'
--- Flickr/flickr_methods.py
+++ Flickr/flickr_methods.py
@@ -10,7 +10,9 @@ photo_url = 'https://api.flickr.com/services/upload'
 
 apikey = '5b5735907b2fc27180f61c658624f3c6'
 apisecret = '1c8a1fe350a2c875'
-callback = 'https://flickr.com'
+callback = 'http://localhost/auth/landing/flickr'
+
+consumer = oauth.Consumer(key=apikey, secret=apisecret)
 
 def get_request_token():
 	
@@ -23,7 +25,6 @@ def get_request_token():
 	'oauth_consumer_key': apikey
 	}
 
-	consumer = oauth.Consumer(key=apikey, secret=apisecret)
 	req = oauth.Request(method="GET", url=token_request_url, parameters=params)
 	req['oauth_signature'] = oauth.SignatureMethod_HMAC_SHA1().sign(req,consumer,None)
 	h = httplib2.Http(".cache")
@@ -37,6 +38,9 @@ def get_request_token():
 	return request_token['oauth_token'], request_token['oauth_token_secret']
 	
 def get_access_token(pin, request_token, request_token_secret):
+	
+	token = oauth.Token(request_token, request_token_secret)
+	token.set_verifier(pin)
 	
 	access_token_parms = {
 	'oauth_consumer_key': apikey,
