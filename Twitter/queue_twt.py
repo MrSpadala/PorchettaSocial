28b7f913466d2d9b5b2cc2f0925e35973d9f0948
Spadala <pietro.spa@gmail.com>
2017-11-26 18:28:42 +0100

    pulizia codice code twitter

--- Twitter/queue_twt.py
+++ Twitter/queue_twt.py
@@ -5,6 +5,8 @@ from rauth import OAuth1Service
 from base64 import b64encode as enc64
 from requests_oauthlib import OAuth1Session
 
+flag = 'ÿ'      #message flag
+
 host_server = 'rabbitmq'
 
 ex = True
@@ -27,66 +29,52 @@ def callback(ch, method, properties, body):
 	channel1 = connection.channel()
 	channel1.queue_declare(queue='to_server', durable=True)
 	message = body.decode('utf-8')
-	l = message.split('ÿ', 2)
-	print("Received msg splitted ",l)
+	l = message.split(flag)
+	print("Received msg_id splitted ",l)
 
-	flag = 'ÿ'
-	msg = l[0]
-	if (l[1] == 'upload_post'):
+	msg_id = l[0]
 	
-		# msg_id ÿ 'upload_post' ÿ access_token ÿ access_tok_secret ÿ text ÿ photo (binary)
-		f = 0
+	if (l[1] == 'upload_post'):
+		# msg_id ÿ 'upload_post' ÿ access_token ÿ access_tok_secret ÿ text ÿ photo (path)
 		consumer_key = 'VyP9pdp6VC1M0qkfS4m14oxqM'
 		consumer_secret = 'udtYapVuIU3vFalBjRmHWIVPPE6yA9BK4Zwzj6XB1kRcg8ekQq'
-		l = message.split('ÿ', 5)
 		access_token = l[2]
 		access_token_secret = l[3]
 		text = l[4]
 		photo = l[5]
-		print(text)
 		flag_photo = False
 		
 		oauth = OAuth1Session(consumer_key, client_secret = consumer_secret,resource_owner_key = access_token,resource_owner_secret = access_token_secret)
 		if len(photo) > 0:
 			flag_photo = True
-			media_info = oauth.post('https://upload.twitter.com/1.1/media/upload.json', data={ 'media_data':enc64(open(photo,'rb').read()) })
-			f = open('DEBUG.txt', 'w')
-			f.write("Media info "+str(media_info)+'\n')
-			f.write("Media info json "+str(media_info.json()))
-			f.close()
+			media_info_resp = oauth.post('https://upload.twitter.com/1.1/media/upload.json', data={ 'media_data':enc64(open(photo,'rb').read()) })
+			media_info = media_info_resp.json()
 			
-			
-			
-			if ( not ('200' in str(media_info))):
-				stringa_invio = msg + flag + 'twtÿupload_postÿ' + 'image error'
+			if ( not ('200' in str(media_info_resp))):
+				stringa_invio = msg_id + flag + 'twtÿupload_postÿ' + 'image error'
 				channel1.basic_publish(exchange='', routing_key = 'to_server', body = stringa_invio)
-			pass
 
 		
 		if (flag_photo):
 			media_info = media_info.json()
 			media_id = media_info['media_id_string']
-			params = {'status': 'testo','media_ids': [media_id]}
+			params = {'status': text, 'media_ids': [media_id]}
 		else:
-			params = {'status': 'testo'}
+			params = {'status': text}
 		
-		params['status']=text
 		r = oauth.post('https://api.twitter.com/1.1/statuses/update.json', data = params,json=None)
 		risposta = 1
-		f = open('DEBUG.txt', 'a')
-		f.write('Risposta upload: '+str(r.json()))
-		f.close()
 		if ('200' in str(r)):
 			risposta = 0
 	
 		if not risposta:
-			stringa_invio =msg+flag+'twtÿupload_postÿ'+str(risposta)
+			stringa_invio =msg_id+flag+'twtÿupload_postÿ'+str(risposta)
 			channel1.basic_publish(exchange='',routing_key = 'to_server',body=stringa_invio)
 		else: 
-			stringa_invio =msg+flag+'twtÿupload_postÿ'+'exception_occurred'
+			stringa_invio =msg_id+flag+'twtÿupload_postÿ'+'exception_occurred'
 			channel1.basic_publish(exchange='',routing_key = 'to_server',body=stringa_invio)
 	else:
-		stringa_invio = msg+ flag + 'twtÿupload_postÿ' + 'unknow command'
+		stringa_invio = msg_id+ flag + 'twtÿupload_postÿ' + 'unknow command'
 		channel1.basic_publish(exchange='', routing_key = 'to_server', body = stringa_invio)
 	
 
